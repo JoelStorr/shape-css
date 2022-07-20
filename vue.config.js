24c9137f9a95566d498c74df23fcf96082b2beb1
Joel Storr <joel.storr@ewe.net>
2022-07-20 19:11:32 +0200

    Edit Git Ignore file

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/shape-css-gp/'
+    : '/',
 });
+
+
+/*
+publicPath: process.env.NODE_ENV === 'production'
+    ? '/shape-css-gp/'
+    : '/',
+*/
