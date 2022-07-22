ae3d80d5887751fc3b35cd2e4e8b2c9a79176259
Joel Storr <joel.storr@ewe.net>
2022-07-22 21:55:00 +0200

    Added Copy interaction + new favicon

--- src/components/Base/BaseCopy.component.vue
+++ src/components/Base/BaseCopy.component.vue
@@ -1,7 +1,8 @@
 <template>
   <section class="code-dispaly" v-copy="copySetter">
     <slot></slot>
-    <div class="overlay">Copy</div>
+    <p class="helper">Click Here to Copy</p>
+    <div class="overlay">Copy <i class="fa-solid fa-copy"></i></div>
   </section>
 </template>
 
@@ -18,13 +19,20 @@ export default {
   margin-top: 50px;
   min-width: 60%;
   max-width: 90%;
-  height: 50px;
+  height: 80px;
   background-color: rgba(0, 0, 0, 0.55);
   border-radius: 5px;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 
+  .helper {
+    color: rgb(187, 187, 187);
+    font-weight: 200;
+    font-size: 0.8rem;
+    padding-top: 10px;
+  }
   .overlay {
     position: absolute;
     top: 0;
@@ -33,7 +41,7 @@ export default {
     right: 0;
     width: 100%;
     height: 100%;
-    background-color: #d19966;
+    background-color: #4a4e69;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -44,8 +52,16 @@ export default {
     transition: opacity 0.5s ease-in-out;
     user-select: none;
 
+    i {
+      padding-left: 10px;
+    }
     &:hover {
       opacity: 1;
+    }
+    &:active {
+      opacity: 1;
+      background-color: #2f3143;
+      color: #b8b8b8;
     }
   }
 }
