4f51314456c7f5fbf8f11a9d9c017a28d342f526
Joel Storr <joel.storr@ewe.net>
2022-07-22 17:14:24 +0200

    Fixed Event Error on Custome Directive

--- src/components/Directives/copy.js
+++ src/components/Directives/copy.js
@@ -1,14 +1,15 @@
-/* eslint-disable indent */
 export default {
-    beforeMount(el, binding) {
-        const copyEl = document.createElement('textarea');
-        copyEl.value = binding.value;
-        copyEl.setAttribute('readonly', '');
-        copyEl.style.position = 'absolute';
-        copyEl.style.left = '-9999px';
-        document.body.appendChild(copyEl);
-        copyEl.select();
-        document.execCommand('copy');
-        document.body.removeChild(copyEl);
-    },
+  mounted(el, binding) {
+    el.addEventListener('click', () => {
+      const copyEl = document.createElement('textarea');
+      copyEl.value = binding.value;
+      copyEl.setAttribute('readonly', '');
+      copyEl.style.position = 'absolute';
+      copyEl.style.left = '-9999px';
+      document.body.appendChild(copyEl);
+      copyEl.select();
+      document.execCommand('copy');
+      document.body.removeChild(copyEl);
+    });
+  },
 };
